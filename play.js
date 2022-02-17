98ca4f5a398c32b453051b2ad6f3dd3a6401ac2e
Kent Gulka <kentgulka@modindsoft.com>
2022-02-17 01:11:14 +0000

    refactored module exports and moved conneciton to file

--- play.js
+++ play.js
@@ -1,17 +1,8 @@
-const net = require("net");
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
- 
-  conn.setEncoding("utf8");
+const myClient = require("./client");
 
-  return conn;
-};
 console.log("Connecting ...");
-const conn = connect();
+const conn = myClient.connect();
 
 const onEventFn = function () {
   console.log('do something');
