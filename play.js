4b3572c6e74379c09a4ba19f14038f9e5980da29
Kent Gulka <kentgulka@modindsoft.com>
2022-02-17 02:43:18 +0000

    added client name

--- play.js
+++ play.js
@@ -5,10 +5,14 @@ console.log("Connecting ...");
 const conn = myClient.connect();
 
 const onEventFn = function () {
-  console.log('do something');
+  console.log('Connected!');
 }
 conn.on("connect", onEventFn);
 
 conn.on('data', (messageFromServer) => {
   console.log('Message from server:', messageFromServer);
+  //send Name: KAG to the server after connection 
+  conn.write("Name: KAG");
 });
+
+
